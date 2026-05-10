6e1725921deb4bb74a5f07fb502b7e4d9bd16577
zhouali311-collab <zhouali311@126.com>
2026-05-10 17:33:38 +0800

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -2,10 +2,28 @@ const exhibitions = [];
 const journalPosts = [];
 const works = [];
 
-export { exhibitions, journalPosts, works };
+// 中文别名，兼容现有页面里的中文 import
+const 展览 = exhibitions;
+const 期刊 = journalPosts;
+const 作品 = works;
+const 文章 = journalPosts;
+
+export {
+  exhibitions,
+  journalPosts,
+  works,
+  展览,
+  期刊,
+  作品,
+  文章,
+};
 
 export default {
   exhibitions,
   journalPosts,
   works,
+  展览,
+  期刊,
+  作品,
+  文章,
 };
