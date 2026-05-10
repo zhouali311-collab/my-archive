8b6b0058bdbf98cadd248018834df9583f113f85
zhouali311-collab <zhouali311@126.com>
2026-05-10 17:15:04 +0800

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,23 @@
-import Navigation from './Navigation'
-import Footer from './Footer'
+import { Outlet } from 'react-router-dom';
+import Navigation from './Navigation';
+import Footer from './Footer';
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col relative">
-      {/* Grain overlay for paper texture */}
+    <div className="min-h-screen bg-cream relative">
+      {/* Grain Overlay */}
       <div className="grain-overlay" aria-hidden="true" />
-      
+
+      {/* Navigation */}
       <Navigation />
-      
-      <main className="flex-1 page-fade-in">
-        {children}
+
+      {/* Main Content */}
+      <main>
+        <Outlet />
       </main>
-      
+
+      {/* Footer */}
       <Footer />
     </div>
-  )
+  );
 }
